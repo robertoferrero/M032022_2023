6e85cf8f550baeb7735514e6088075fa2b750fa8
rferrero <rferrero@profe>
2022-11-29 12:32:54 +0100

    Canvi de package

--- src/cat/copernic/m03/uf1a4/Ex09_ComptaPepe.java
+++ src/cat/copernic/m03/uf1a4/Ex09_ComptaPepe.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package aceptaelreto;
+package cat.copernic.m03.uf1a4;
 
 import java.util.Scanner;
 
