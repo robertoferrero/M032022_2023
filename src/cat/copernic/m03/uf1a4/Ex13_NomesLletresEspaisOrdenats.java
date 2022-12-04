3425d49ffb242f2f2baf0724a243d1ad86b423af
rober <rober@192.168.1.132>
2022-12-04 19:50:55 +0100

    Ex13_NomesLletresEspaisOrdenats

---
+++ src/cat/copernic/m03/uf1a4/Ex13_NomesLletresEspaisOrdenats.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cat.copernic.m03.uf1a4;
+
+/**
+ *
+ * @author rober
+ */
+public class Ex13_NomesLletresEspaisOrdenats {
+    
+    public static void main(String[] args) {
+    
+        String prova = "public static void mainString args ";
+
+        String cadenaOrdenada = prova.substring(0,1);
+
+        for (int i = 1; i < prova.length(); ++i) {  
+            String lletraAInsertar = prova.substring(i,i+1);
+            int j = 0;
+            while(j < cadenaOrdenada.length() && 
+                  lletraAInsertar.compareTo(cadenaOrdenada.substring(j)) > 0)
+                j++;
+            
+            if (j < cadenaOrdenada.length())
+                cadenaOrdenada = cadenaOrdenada.substring(0,j) + lletraAInsertar + 
+                        cadenaOrdenada.substring(j);
+            else
+                cadenaOrdenada += lletraAInsertar;
+        }
+        
+        // Obtenim el total d'espais al principi de la cadena
+        int espais = 0 , index = 0;
+        while (cadenaOrdenada.charAt(index) == ' ')
+        {
+                ++espais;
+                ++index;
+        }
+        // Recol·loquem els espais al final
+        cadenaOrdenada = cadenaOrdenada.trim() + cadenaOrdenada.substring(0,espais);
+        System.out.println(cadenaOrdenada);
+    }
+
+    
+}
